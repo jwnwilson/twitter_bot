1130241fd863a356b83071ea31d948c37e0cae70
Noel Wilson <noelwilson@noelwilson-macbookair2.roam.corp.google.com>
2016-02-02 21:49:31 +0000

    Setting up tests

--- twitter_bot/tests/functional_tests.py
+++ twitter_bot/tests/functional_tests.py
@@ -4,7 +4,12 @@ import logging
 import time
 from datetime import datetime
 from django.test import LiveServerTestCase
-from twitter_bot.models import HashTag, HashTagBattle
+from twitter_bot.models import HashTag
+from twitter_bot.models import HashTagBattle
+from twitter_bot.common import get_hash_tag_typos
+from twitter_bot.common import get_tweets_by_hash_tag
+from twitter_bot.common import get_tweet_typos
+from twitter_bot.common import get_hash_tag_most_typos
 
 
 class CCViewsTestCase(LiveServerTestCase):
@@ -28,7 +33,7 @@ class CCViewsTestCase(LiveServerTestCase):
         hash_tag_2 = HashTag.objects.get(hash_tag=hash_tag_name_2)
         hash_tag_battle = HashTagBattle.objects.get(hash_tags__hash_tag__in=[hash_tag_1,hash_tag_2])
 
-        self.assert(hash_tag_battle,)
+        self.assertTrue(hash_tag_battle,)
 
     def test_get_hash_tag_tweets_between_times(self):
         """
@@ -38,7 +43,7 @@ class CCViewsTestCase(LiveServerTestCase):
         hash_tag = "dogs"
         start_date = datetime.date()
         end_date = datetime.date()
-        tweets = get_hash_tag_tweets(hash_tag, start_date, end_date)
+        tweets = get_tweets_by_hash_tag(hash_tag, start_date, end_date)
 
         self.assertEqual(len(tweets), 0)
 
@@ -48,7 +53,7 @@ class CCViewsTestCase(LiveServerTestCase):
         :return:
         """
         test_message = "cat dog thn that hat kool"
-        typos = get_typos_in_string(test_message)
+        typos = get_tweet_typos(test_message)
 
         self.assertEqual(len(typos), 2)
 
@@ -60,10 +65,25 @@ class CCViewsTestCase(LiveServerTestCase):
         hash_tag = "dogs"
         start_date = datetime.date()
         end_date = datetime.date()
-        typos = get_hash_tag_typos_start_end(hash_tag, start_date, end_date)
+        typos = get_hash_tag_most_typos(hash_tag, start_date, end_date)
 
         self.assertEqual(len(typos), 0)
 
+    def test_get_hash_tag_typos(self):
+        """
+        Check that hash tag battle returns expected results
+        :return:
+        """
+        hash_tag_name_1 = "dogs"
+        hash_tag_name_2 = "cats"
+
+        hash_tag_1 = HashTag.objects.get(hash_tag=hash_tag_name_1)
+        hash_tag_2 = HashTag.objects.get(hash_tag=hash_tag_name_2)
+        hash_tag_battle = HashTagBattle.objects.get(hash_tags__hash_tag__in=[hash_tag_1,hash_tag_2])
+
+        typos = get_hash_tag_typos(hash_tag_battle)
+
+        self.assertEqual(len(typos), 0)
 
 if __name__ == "__main__":
     unittest.main()
